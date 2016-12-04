8d859e48681f0756f4f47bfdd39c6ce69dafb854
Yulia  Zileeva <zileeva.y@husky.neu.edu>
2016-12-04 16:05:54 -0500

    Dictionary tagger and simple sentiment score

--- src/api/evaluate.py
+++ src/api/evaluate.py
@@ -1,12 +1,15 @@
 import re, math, collections, itertools
 import nltk
+from nltk.corpus import wordnet
+from nltk.corpus import sentiwordnet as swn
+import yaml
 
 def _get_sentences(is_positive):
     if is_positive:
-        data = open('../../dataset/positives.txt', 'r')
+        data = open('dataset/positives.txt', 'r')
         pos_or_neg = '1'
     else:
-        data = open('../../dataset/negatives.txt', 'r')
+        data = open('dataset/negatives.txt', 'r')
         pos_or_neg = '0'
 
     data_array = []
@@ -40,6 +43,48 @@ class POSTagger(object):
         pos = [[(word, word, [postag]) for (word, postag) in sentence] for sentence in pos]
         return pos
 
+class DictionaryTagger(object):
+
+    def tag(self, postagged_sentences):
+        return [self.tag_sentence(sentence) for sentence in postagged_sentences]
+
+    def tag_sentence(self, sentence, tag_with_lemmas=False):
+        tag_sentence = []
+
+        for (word, lemma, postag) in sentence:
+            tag = self.wordnet_pos_code(postag[0])
+            senti_word = swn.senti_synsets(word)
+            pos_score = 0.0
+            neg_score = 0.0
+            for sw in senti_word:
+                pos_score += sw.pos_score()
+                neg_score += sw.neg_score()
+            s = (word, lemma, postag, pos_score, neg_score)
+            tag_sentence.append(s)
+        return tag_sentence
+
+    # Translation from nltk to Wordnet
+    def wordnet_pos_code(self, tag):
+        if tag.startswith('NN'):
+            return wordnet.NOUN
+        elif tag.startswith('VB'):
+            return wordnet.VERB
+        elif tag.startswith('JJ'):
+            return wordnet.ADJ
+        elif tag.startswith('RB'):
+            return wordnet.ADV
+        else:
+            return ''
+
+def sentiment_score(sentence):    
+    sentence = sentence[0]
+    score = 0
+    for (word, lemma, postag, pos_score, neg_score) in sentence:
+        score += pos_score
+        score -= neg_score
+
+    return score
+
 if __name__ == "__main__":
     # Positives and negative sets
     pos_data = _get_sentences(True)
@@ -48,7 +93,12 @@ if __name__ == "__main__":
     # New Split and Tagger object
     splitter = Splitter()
     pos_tagger = POSTagger()
+    dicttagger = DictionaryTagger()
+
+    # pos_tagged_sentences = [[('Every', 'Every', ['DT']), ('time', 'time', ['NN']), ('I', 'I', ['PRP']), ('eat', 'eat', ['VBP']), ('here', 'here', ['RB']), (',', ',', [',']), ('I', 'I', ['PRP']), ('see', 'see', ['VBP']), ('caring', 'caring', ['VBG']), ('teamwork', 'teamwork', ['NN']), ('to', 'to', ['TO']), ('a', 'a', ['DT']), ('professional', 'professional', ['JJ']), ('degree', 'degree', ['NN']), ('.', '.', ['.'])]]
+    # print dicttagger.tag(pos_tagged_sentences)
 
     # Go through the sentences and tag
     for sentence in pos_data:
-        print pos_tagger.pos_tag(splitter.split(sentence))
+        tagged_sentence = dicttagger.tag(pos_tagger.pos_tag(splitter.split(sentence)))
+        print sentence, sentiment_score(tagged_sentence)
